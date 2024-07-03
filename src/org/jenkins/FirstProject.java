2af502529e6f4f164fe614f7640721910cd5aee3
abishekramasamy <abishekramasamy19@gmail.com>
2024-07-03 10:00:45 +0530

    New File
    Signed-off-by: abishekramasamy <abishekramasamy19@gmail.com>

---
+++ src/org/jenkins/FirstProject.java
@@ -0,0 +1,13 @@
+package org.jenkins;
+
+public class FirstProject {
+	public static void main(String[] args) {
+		
+		System.out.println("Abishek");
+		System.out.println("Mahe");
+		System.out.println("Moni");
+		System.out.println("Guru");
+		
+	}
+
+}
